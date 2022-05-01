ec45edca7f7d4889ae817672a377c1bd04e2803f
Pat Trudel <patrick.trudel@me.com>
2022-05-01 12:18:44 -0400

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,6 @@ git checkout -b main
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:itsPat/itsPat.github.io.git main
 
 cd -
